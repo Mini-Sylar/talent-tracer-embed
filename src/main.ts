be0cc8a3bcf8214b46e5d055c5190e9574081f27
Mini-Sylar <minisylar3@gmail.com>
2023-11-25 23:02:38 +0000

    chore: clean base app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import './assets/main.css'
-
+import 'primevue/resources/themes/lara-light-green/theme.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue)
 
 app.mount('#app')
