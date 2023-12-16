5d9581eda735473be830ded78ef9c46e9176ff17
Mini-Sylar <minisylar3@gmail.com>
2023-12-16 14:00:44 +0000

    Add custom field app state

--- src/stores/app_state.ts
+++ src/stores/app_state.ts
@@ -4,10 +4,16 @@ export const useAppStateStore = defineStore('app_state', () => {
   const urlParams = ref(new URLSearchParams(window.location.search))
   const job_id = computed(() => urlParams.value.get('job_id'))
   const isCustomMode = computed(() => urlParams.value.get('custom_mode'))
+  const customFields = computed(
+    () =>
+      urlParams.value.get('custom_fields') &&
+      JSON.parse(urlParams.value.get('custom_fields') as string)
+  )
+  console.log(customFields.value)
   const showDescription = computed(() => urlParams.value.get('job_description'))
   const theme = computed(() => urlParams.value.get('theme') || 'lara-light-green')
 
-  return { job_id, isCustomMode, showDescription, theme }
+  return { job_id, isCustomMode, showDescription, theme, customFields }
 })
 
 if (import.meta.hot) {
